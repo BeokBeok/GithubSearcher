62fc4b7027df64e924ec55d0005e6519cdc0eaff
BeokBeok <kekemusa37@nate.com>
2019-08-10 13:14:17 +0900

    메인 레이아웃 구조 작성

---
+++ app/src/main/java/com/githubsearcher/githubsearchlike/SearchLikeContentsFragment.kt
@@ -0,0 +1,10 @@
+package com.githubsearcher.githubsearchlike
+
+
+import com.githubsearcher.R
+import com.githubsearcher.base.BaseFragment
+import com.githubsearcher.databinding.FragmentSearchLikeContentsBinding
+
+class SearchLikeContentsFragment : BaseFragment<FragmentSearchLikeContentsBinding>(
+    R.layout.fragment_search_like_contents
+)
