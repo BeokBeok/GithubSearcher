a27aa30e3ccc322206f03b8cbcb723919644f4f9
BeokBeok <kekemusa37@nate.com>
2019-08-10 11:38:47 +0900

    데이터바인딩 사용하도록 설정

--- app/src/main/java/com/githubsearcher/base/BaseFragment.kt
+++ app/src/main/java/com/githubsearcher/base/BaseFragment.kt
@@ -5,20 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment(
+abstract class BaseFragment<VDB : ViewDataBinding>(
     @LayoutRes
     private val resource: Int
 ) : Fragment() {
+
+    private lateinit var binding: VDB
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(
-        resource,
-        container,
-        false
-    )
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            resource,
+            container,
+            false
+        )
+        return binding.root
+    }
 }
--- app/src/main/java/com/githubsearcher/githubsearchlike/SearchLikeOutlineFragment.kt
+++ app/src/main/java/com/githubsearcher/githubsearchlike/SearchLikeOutlineFragment.kt
@@ -3,7 +3,8 @@ package com.githubsearcher.githubsearchlike
 
 import com.githubsearcher.R
 import com.githubsearcher.base.BaseFragment
+import com.githubsearcher.databinding.FragmentSearchLikeBinding
 
-class SearchLikeOutlineFragment : BaseFragment(
+class SearchLikeOutlineFragment : BaseFragment<FragmentSearchLikeBinding>(
     R.layout.fragment_search_like
 )
