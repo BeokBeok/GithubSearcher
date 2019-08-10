ceb5000a27ec8f47c9a195fd52fdcebc208da300
BeokBeok <kekemusa37@nate.com>
2019-08-10 22:43:24 +0900

    좋아요 체크박스 버튼 추가

--- app/src/main/java/com/githubsearcher/data/UsersDetail.kt
+++ app/src/main/java/com/githubsearcher/data/UsersDetail.kt
@@ -4,19 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 data class UsersDetail(
 
-    // 아바타 이미지
     @SerializedName("avatar_url")
     val avatarUrl: String,
 
-    // 이름
     @SerializedName("login")
     val login: String,
 
-    // 스코프
     @SerializedName("score")
-    val score: Double,
-
-    // 좋아요 여부를 포함한 아이템
-    @SerializedName("starred_url")
-    val starredUrl: String
+    val score: Double
 )
