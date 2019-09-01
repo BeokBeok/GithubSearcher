d03809187f0f6b0dc1eebbe3b77a3d344381a736
BeokBeok <kekemusa37@nate.com>
2019-09-01 20:58:11 +0900

    onCleared 호출 시 dispose 처리하도록 변경

--- app/src/main/java/com/githubsearcher/base/BaseViewModel.kt
+++ app/src/main/java/com/githubsearcher/base/BaseViewModel.kt
@@ -4,11 +4,13 @@ import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
-abstract class BaseViewModel : ViewModel() {
+open class BaseViewModel : ViewModel() {
     private val compositeDisposable = CompositeDisposable()
 
     override fun onCleared() {
-        compositeDisposable.clear()
+        if (!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
         super.onCleared()
     }
 
